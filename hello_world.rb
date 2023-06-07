02b2e812ce96bb3e489e16dddcbfb05f590165f0
Abi Anusha <abianusha.s@rorwebcoders.com>
2023-06-07 18:15:59 +0530

    First commit

---
+++ hello_world.rb
@@ -0,0 +1,27 @@
+print "Hello world! \n" 
+print "Am from rb file\n"
+
+puts "Enter Your name"
+name=gets()
+puts"\nHi #{name} \n\n"
+
+
+END {
+	puts "Inside end"
+}
+
+name = "mat" \
+		+"z"
+puts name
+
+print <<`CMD`
+	echo hello world
+CMD
+
+BEGIN {
+	puts "Inside begin"
+}
+
+
+
+
